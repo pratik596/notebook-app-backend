a4a94bf31ffc87507d3ba584c37cac6014dfdf22
pratiksarvadhi <pratik@sarvadhi.com>
2023-09-13 18:11:26 +0530

    aaded local change

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+ 
+
+from dotenv import load_dotenv
+
+ 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +33,8 @@ SECRET_KEY = "django-insecure-rivc#4(rrps+%1@tpc^l^c4)_-=5kpkl(px8kgt=#)h_8+0p%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split() if os.getenv(
+    "ALLOWED_HOSTS") else ["*"]
 
 # Application definition
 
